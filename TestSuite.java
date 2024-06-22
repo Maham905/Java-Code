989f36f29fa48989da120409ab129d3915f318c2
Maham905 <nasirmaham905@gmail.com>
2024-06-22 15:06:03 +0500

    Create TestSuite.java

---
+++ TestSuite.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    VoterTest.class,
+    // Add other test classes here
+})
+public class TestSuite {
+    // the class remains empty,
+    // used only as a holder for the above annotations
+}
