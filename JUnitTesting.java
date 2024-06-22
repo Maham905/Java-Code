406d996880432b7d4ccd43c2c806016efd62c879
Maham905 <nasirmaham905@gmail.com>
2024-06-22 15:55:23 +0500

    Update JUnitTesting.java

--- JUnitTesting.java
+++ JUnitTesting.java
@@ -1,3 +1,4 @@
+//VoterTest
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -9,3 +10,19 @@ public class VoterTest {
         assertEquals("12345", voter.getId());
     }
 }
+//FactorialCalculatorTest
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class FactorialCalculatorTest {
+    @Test
+    public void testFactorial() {
+        FactorialCalculator calculator = new FactorialCalculator();
+        assertEquals(1, calculator.factorial(1));
+        assertEquals(2, calculator.factorial(2));
+        assertEquals(6, calculator.factorial(3));
+        assertEquals(24, calculator.factorial(4));
+        assertEquals(120, calculator.factorial(5));
+    }
+}
+
